e3d626ca2cd693832c8e89fb91608fddaa2ab5d3
H7-26 <92584178+H7-26@users.noreply.github.com>
2026-02-21 01:57:18 +0100

    switch mpv source

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -23,7 +23,7 @@ ExternalProject_Add(mpv
         vapoursynth
         libsdl2
         subrandr
-    GIT_REPOSITORY https://github.com/mpv-player/mpv.git
+    GIT_REPOSITORY https://github.com/H7-26/mpv.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
