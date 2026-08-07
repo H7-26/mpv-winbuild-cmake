9ce2e82a3ad63269700f30833780c1fa243f51e8
H7-26 <92584178+H7-26@users.noreply.github.com>
2026-08-07 13:30:57 +0200

    Update openssl.cmake

--- packages/openssl.cmake
+++ packages/openssl.cmake
@@ -8,7 +8,7 @@ ExternalProject_Add(openssl
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
     GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !test"
     GIT_REMOTE_NAME origin
-    GIT_RESET d8bf6cdd4849925c30e4f1911c7acb49cb34b702
+    GIT_RESET 5be1a2a1432947ca0a381848d71f5e852d85929d
     GIT_SUBMODULES ""
     UPDATE_COMMAND ""
     PATCH_COMMAND ${EXEC} git am --3way ${CMAKE_CURRENT_SOURCE_DIR}/openssl-*.patch
